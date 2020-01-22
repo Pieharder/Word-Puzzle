b4f68af6ba2e73628c48f0215b35f9c65f371d03
Geoff Goetz and Matt Taylor <matt@>
2020-01-22 11:50:02 -0800

    Create initial files. Add HTML and JS code.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $("form#input").submit(function(event) {
+    event.preventDefault();
+    var words = $("input#wordInput").val();
+    var letters = words.split("");
+    var replacedLetters = [];
+    console.log(words);
+    for (var index = 0; index < letters.length; index += 1) {
+      if(letters[index] === "a" || letters[index] === "e" || letters[index] === "i" || letters[index] === "o" || letters[index] === "u") {
+        // alert("test!");
+        letters.splice(index, 1, "-");
+      console.log(letters);
+      };
+    };
+
+    letters.forEach(function(letter) {
+      replacedLetters.push(letter);
+    })
+    // replacedLetters.push(letters);
+    // console.log(replacedLetters);
+    var result = replacedLetters.join("");
+    console.log(result);
+    $(".final").text(result);
+  });
+});
+
+
+// letters.splice(0, 1, "-");
